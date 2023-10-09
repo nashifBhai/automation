1abd4f88275471cd6b1802572431c6dacd09f482
Md. Nashif Iftekhar <nashififtekhar@gmail.com>
2023-10-09 14:47:12 +0600

    Revert "start"
    This reverts commit f3eae0f046547c8f2b14bfe575dfb274e7740666.

--- cypress/e2e/Test Case 1/testCase1_spec.cy.js
+++ cypress/e2e/Test Case 1/testCase1_spec.cy.js
@@ -1,56 +1,65 @@
-const homePage = require("../page_objects/home.page");
-const loginPage = require("../page_objects/login.page");
-const productPage = require("../page_objects/products.page");
+const loginPageObjs = require("../page_objects/login.page");
+const homePageObjs = require("../page_objects/home.page");
+const myInfoPageObjs = require("../page_objects/myInfo.page");
+const testData = require("../../fixtures/credentials.json");
 
-beforeEach(() => {
+beforeEach({retries: { runMode: 2, openMode: 1,},},() => {
     // open the application and verify elements
-    cy.visit('https://www.automationexercise.com/');
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    //verify login resource
+    cy.loginResource();
 });
 
-describe("Automation Excercise Project Solution", () => {
-    it("Home page visibility", () => {
-        //home page assertions
-        homePage.homePageAssertions();
-    });
-    it("Signup page", () => {
-        //login signup assertions
-        loginPage.loginSignupAssertion();
-        //signup button
-        loginPage.signUp();
-        //fill up signup details
-        loginPage.signUpDetails();
+afterEach(() => {
+    // ensure clean test slate for these tests
+    //cy.then(Cypress.session.clearCurrentSessionData);
+});
+
+describe("Login Verification for User", () => {
+    it("Check Login Pages for Wrong credentials", {retries: { runMode: 2, openMode: 1,},}, () => {
+        //retries for retry attempt to run again
+        loginPageObjs.loginWrongFunctionality();
     });
 
-    it("Login with Wrong Credentials", () => {
-        //home page assertions
-        homePage.homePageAssertions();
-        //login with wrong Credentials
-        loginPage.loginSignupAssertion();
-        //login with wrong credentials
-        loginPage.loginWrongCredentials();
+    it("Check Login Pages for right credentials", () => {
+        //login with general approach
+        loginPageObjs.loginFunctionality();
+        //intercept with link
+        cy.intercept('https://opensource-demo.orangehrmlive.com/web/index.php/events/push').as('login');
+        //cy.wait(20000) // default request Timeout will be overwritten by this value
+        cy.wait('@login');
+        cy.wait(3000);
+        homePageObjs.homePageAssertions();
+        //intercept with api
+        cy.intercept('Get', '/web/index.php/*').as('indexPage');
+        //cy.wait('@indexPage');
+        //cy.then(Cypress.session.clearCurrentSessionData);
+        // click on my InfoMenu
+        //homePageObjs.myInfoMenuClicked();
+        cy.wait(10);
+    });
+});
+
+describe("Update of My Info", () => {
+    it("Add information of user", () => {
+        //login with Json Data
+        loginPageObjs.loginFunctionalityJson(testData.username, testData.password);
+        // click on my Info Menu
+        homePageObjs.myInfoMenuClicked();
+        // add data to My Info
+        myInfoPageObjs.addMyInfo();
+        //reload the page
+        cy.reload();
+        // download file
+        myInfoPageObjs.downloadFile();
+        //remove file
+        myInfoPageObjs.fileRemove();
+        // read file
+        myInfoPageObjs.fileRead();
     });
 
-    it("Login with Right Credentials", () => {
-        //home page assertions
-        homePage.homePageAssertions();
-        //login with wrong Credentials
-        loginPage.loginSignupAssertion();
-        //login with wrong credentials
-        loginPage.loginRightCredentials();
-        //home page assertions
-        homePage.homePageAssertions();
-    });
-
-    it("Search a product and add to cart the Product", () => {
-        //home page assertions
-        homePage.homePageAssertions();
-        //login with wrong Credentials
-        loginPage.loginSignupAssertion();
-        //login with wrong credentials
-        loginPage.loginRightCredentials();
-        //home page assertions
-        homePage.homePageAssertions();
-        productPage.productsPageAssertion();
-        productPage.searchProduct();
+    it("JQuery", () => {
+        //login with JQuery
+        loginPageObjs.loginFunctionalityJquery();
     });
 });
